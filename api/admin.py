32f6c3b9f5f4ededc198aee265c9cc9be61aa7a3
cristian <cfpena@espol.edu.ec>
2016-05-26 19:50:52 -0500

    creacion de modelos kit, item

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from .models import Item,Kit
+
+admin.site.register(Item)
+admin.site.register(Kit)
+
 # Register your models here.
--- api/models.py
+++ api/models.py
@@ -1,3 +1,35 @@
 from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+@python_2_unicode_compatible
+class Kit(models.Model):
+    codigo= models.CharField(max_length=10)
+    tieneSerie= models.BooleanField(default=False)
+    serie= models.CharField(max_length=20,null=True)
+    nombre = models.CharField(max_length=20)
+    descripcion = models.CharField(max_length=50,null=True)
+    fechaCreacion= models.DateTimeField(null=True)
+    fechaActualizacion= models.DateTimeField(null=True)
+
+    def __str__(self):
+        return self.nombre
+
+
+
+@python_2_unicode_compatible
+class Item(models.Model):
+    codigo= models.CharField(max_length=10)
+    tieneSerie= models.BooleanField(default=False)
+    serie= models.CharField(max_length=20,null=True)
+    nombre = models.CharField(max_length=20)
+    descripcion = models.CharField(max_length=50,null=True)
+    fechaCreacion= models.DateTimeField(null=True)
+    fechaActualizacion= models.DateTimeField(null=True)
+    kit = models.ForeignKey(Kit,on_delete=models.CASCADE, null=True)
+
+    class Meta:
+        ordering = ('nombre',)
+    def __str__(self):
+        return self.nombre
+
 
 # Create your models here.
