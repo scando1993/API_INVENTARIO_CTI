2e62af575402248553e56e5d68539cbfc887406c
kevincando <scando@fiec.espol.edu.ec>
2016-06-15 14:37:03 -0500

    Cambios al capfile y al deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,23 +2,31 @@
 lock '3.5.0'
 
 set :application, 'sistemaDeInventarioCTI-API'
-set :repo_url, 'git@example.com:me/my_repo.git'
+set :repo_url, 'https://github.com/cfpena/sistemaDeInventarioCTI-API.git'
 
 # Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+ask :branch, "master"
 
 # Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
+set :deploy_to, '/var/www/sistemaInventarioCTI-API/code'
 
 # Default value for :scm is :git
-# set :scm, :git
+set :scm, :git
+
+set :user, "sistemaInventarioCTI-API"
+
+set :use_sudo, false
+
+set :rails_env, "production"
+
+set :deploy_via, :copy
 
 # Default value for :format is :airbrussh.
 # set :format, :airbrussh
 
 # You can configure the Airbrussh format using :format_options.
 # These are the defaults.
-# set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
+set :format_options, command_output: true, log_file: 'log/capistrano.log', color: :auto, truncate: :auto
 
 # Default value for :pty is false
 # set :pty, true
@@ -33,17 +41,17 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
 # Default value for keep_releases is 5
-# set :keep_releases, 5
+set :keep_releases, 5
 
 namespace :deploy do
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
+	after :restart, :clear_cache do
+		on roles(:web), in: :groups, limit: 3, wait: 10 do
+			# Here we can do anything such as:
+			# within release_path do
+			#   execute :rake, 'cache:clear'
+			# end
+		end
+	end
 
 end
