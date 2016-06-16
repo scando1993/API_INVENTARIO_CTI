e8876019875564f2ad52c7b361cf72b1cf189a3e
kevincando <scando@fiec.espol.edu.ec>
2016-06-16 02:01:21 -0500

    Cambios al puma server para poder manejar ambos ambientes development como production

--- config/puma.rb
+++ config/puma.rb
@@ -13,7 +13,8 @@ port        ENV.fetch("PORT") { 3000 }
 
 # Specifies the `environment` that Puma will run in.
 #
-environment ENV.fetch("RAILS_ENV") { "development" }
+rails_env = ENV.fetch("RAILS_ENV") || "development"
+environment rails_env
 
 # Specifies the number of `workers` to boot in clustered mode.
 # Workers are forked webserver processes. If using threads and workers together
@@ -21,7 +22,7 @@ environment ENV.fetch("RAILS_ENV") { "development" }
 # Workers do not work on JRuby or Windows (both of which do not support
 # processes).
 #
-workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+#workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 
 # Use the `preload_app!` method when specifying a `workers` number.
 # This directive tells Puma to first boot the application and load code
@@ -30,7 +31,7 @@ workers ENV.fetch("WEB_CONCURRENCY") { 2 }
 # you need to make sure to reconnect any threads in the `on_worker_boot`
 # block.
 #
-preload_app!
+#preload_app!
 
 # The code in the `on_worker_boot` will be called if you are using
 # clustered mode by specifying a number of `workers`. After each worker
@@ -45,3 +46,29 @@ preload_app!
 
 # Allow puma to be restarted by `rails restart` command.
 plugin :tmp_restart
+
+if rails_env == "production"
+	app_dir = File.expand_path("../..",__FILE__)
+	shared_dir = "#{app_dir}/shared"
+
+	# Set up socket location
+	bind "unix://#{shared_dir}/sockets/puma.sock"
+
+	# Logging
+	stdout_redirect "#{shared_dir}/log/puma.stdout.log", "#{shared_dir}/log/puma.stderr.log", true
+
+	# Set master PID and state locations
+	pidfile "#{shared_dir}/pids/puma.pid"
+	state_path "#{shared_dir}/pids/puma.state"
+	activate_control_app
+
+	on_worker_boot do
+		require "active_record"
+		ActiveRecord::Base.connection.disconnect! rescue ActiveRecord::ConnectionNotEstablished
+		ActiveRecord::Base.establish_connection(YAML.load_file("#{app_dir}/config/database.yml")[ENV.fetch('RAILS_ENV')])
+	end
+
+	workers ENV.fetch("WEB_CONCURRENCY") { 2 }
+
+	preload_app!
+end
