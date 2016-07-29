11aa84920139e2caa79dea62f859a588b963277e
kevin cando <scando@fiec.espol.edu.ec>
2016-07-28 21:13:25 -0500

    Se borro a los serializadores

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,12 +1,12 @@
-class ItemSerializer < ActiveModel::Serializer
-  attributes :id,:title,:code,:state,:reference,:domain
-
-  has_many :kits, :through => :kit_items
-  has_many :items, :through => :item_components
-
-  has_many :kit_items
-  has_many :item_components
-
-  has_many :owners
-  # has_and_belongs_to_many :owners
-end
+# class ItemSerializer < ActiveModel::Serializer
+#   attributes :id,:title,:code,:state,:reference,:domain
+#
+#   has_many :kits, :through => :kit_items
+#   has_many :items, :through => :item_components
+#
+#   has_many :kit_items
+#   has_many :item_components
+#
+#   has_many :owners
+#   # has_and_belongs_to_many :owners
+# end
--- app/serializers/kit_serializer.rb
+++ app/serializers/kit_serializer.rb
@@ -1,15 +1,15 @@
-class KitSerializer < ActiveModel::Serializer
-  attributes :id, :title,:number_elements,:code,:kit_type,:state,:reference,:domain,:serie
-
-  has_many :items, :through => :kit_items, source: :item
-
-  has_many :kits, :through => :kit_components, source: :kit_component
-
-  has_many :kit_components
-
-  has_many :kit_items
-
-  has_many :kit_comments, dependent: :destroy
-
-  has_many :owners
-end
+# class KitSerializer < ActiveModel::Serializer
+#   attributes :id, :title,:number_elements,:code,:kit_type,:state,:reference,:domain,:serie
+#
+#   has_many :items, :through => :kit_items, source: :item
+#
+#   has_many :kits, :through => :kit_components, source: :kit_component
+#
+#   has_many :kit_components
+#
+#   has_many :kit_items
+#
+#   has_many :kit_comments, dependent: :destroy
+#
+#   has_many :owners
+# end
